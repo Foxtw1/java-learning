8acda0be1448546c1197ab606f026bedcfa2b73f
Trevor William Fox <foxtw@s.dcsdk12.org>
2018-08-20 08:24:53 -0600

    Average of rainfall in April, May, and June

---
+++ chapter10/E1.java
@@ -0,0 +1,16 @@
+class E1 {
+
+
+    public static void main(String[] args){
+
+
+        int april = 12;
+        int may = 14;
+        int june = 8;
+
+
+
+        System.out.println((may + april + june) / 3.0);
+
+    }
+}
