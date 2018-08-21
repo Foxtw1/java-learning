607bf3de888eaa6a38b50f12562306b05d4139b5
Trevor William Fox <foxtw@s.dcsdk12.org>
2018-08-21 08:28:05 -0600

    adds sin^2 + cos^2 = 1

---
+++ chapter10/E2.java
@@ -0,0 +1,15 @@
+class E2 {
+
+public static void main(String[] args){
+        
+        double a = Math.sin(0.5236);
+        double b = Math.cos(0.5236);
+        double c = (a * a) + (b * b);
+
+        System.out.println("sine: " + a + " cos: " + b + " sum of squares: " + c);
+
+    }
+
+
+
+}
