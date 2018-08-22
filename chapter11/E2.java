5a2a0c0c603ac00de910cad731f671ab28047548
Trevor William Fox <foxtw@s.dcsdk12.org>
2018-08-22 08:23:05 -0600

    can count the amount in strings

---
+++ chapter11/E2.java
@@ -0,0 +1,16 @@
+public class E2 {
+
+    public static void main (String[] args) {
+        String str;
+        int len;
+        str = new String ("Elementary, my dear Watson! fja;s;gjosfdk'pgk'sdjm;");
+        len = str.length();
+        System.out.println("The length is: " + len);
+
+
+    }
+
+
+
+
+}
