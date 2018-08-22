b051c52efeab0779a83bdedc0eb668e81c6975b2
Trevor William Fox <foxtw@s.dcsdk12.org>
2018-08-22 08:28:30 -0600

    trims the string down in the front and the back

---
+++ chapter11/E3.java
@@ -0,0 +1,15 @@
+class E3 {
+
+    public static void main (String[] args){
+
+        String first = new String("   in a hole   in the ground there lived a hobbit.");
+
+        String second;
+        second = first.trim();
+        System.out.println(second);
+
+    }
+
+
+
+}
