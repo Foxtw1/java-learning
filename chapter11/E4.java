3a61a76f4a7d526f7f6ed2676258ec70dce416eb
Trevor William Fox <foxtw@s.dcsdk12.org>
2018-08-22 08:35:30 -0600

    can cut out first 6 digits out of a string

---
+++ chapter11/E4.java
@@ -0,0 +1,13 @@
+class E4 {
+
+    public static void main (String[] args) {
+
+        String str = new String("golf is a good walk spoiled.");
+        String sub = str.substring(6);
+        System.out.println(sub);
+
+
+    }
+
+
+}
