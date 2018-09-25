257cde39d4eb96c425cef846e9e1ba9079d8ffa8
Trevor Fox <foxtw@s.dcsdk12.org>
2018-09-25 08:27:56 -0600

    scans user for upper and lower limit, still need to do 24 E4

---
+++ chapter25/E3.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class E3 {
+
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is the upper limit?");
+        int upper = scan.nextInt();
+
+        System.out.println("What is the lower limit?");
+        int lower = scan.nextInt();
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
