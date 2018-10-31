3310f4cc3de197eb811f49315d6f7dbd3a8e0fc8
Trevor Fox <foxtw@s.dcsdk12.org>
2018-10-31 08:32:45 -0600

    Can evaluate numbers

---
+++ chapter54/E4.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+import javax.xml.transform.stax.StAXSource;
+
+public class E4{
+
+
+    public static void main(String[] args) {
+        
+
+        int i = 0;
+        while(i == 0){
+            Scanner scan = new Scanner(System.in);
+            System.out.println("What is the number? Input negative number to quit.");
+            int number = scan.nextInt();
+            
+            if(number < 0){
+                break;
+            }
+
+
+            System.out.println(findPerfection(number));
+
+        }
+
+
+
+
+
+
+
+    }
+
+
+    public static boolean findPerfection(int number){
+
+        int total = 0;
+        for(int i = 1;i < number;i++){
+
+            if(number % i == 0){
+                total = total + i;
+
+            }
+
+
+        }
+
+        if(total == number){
+            return true;
+        }
+        return false;
+
+
+
+
+    }
+
+
+
+}
